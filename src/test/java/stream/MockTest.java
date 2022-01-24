440bcaccfef00bd52f6028e6e0237152ee8f7c2e
goodjwon <firstman9@protonmail.com>
2022-01-24 09:11:36 +0900

    collection 테스트를 위한 테스트 문제

--- src/test/java/stream/MockTest.java
+++ src/test/java/stream/MockTest.java
@@ -1,10 +1,14 @@
 package stream;
 
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.Matchers.*;
 
 public class MockTest {
 
@@ -13,12 +17,24 @@ public class MockTest {
     public void testMockData() throws IOException {
         List<Car> cars = MockData.getCars();
 
+        MatcherAssert.assertThat(cars.size(), greaterThan(0));
     }
 
     @Test
-    @DisplayName("파란색 모델만 구해서 이름과 색상을 출력하세요.")
+    @DisplayName("파란색 색상을 구해서 모델을 출력하세요.")
     public void testGetModel() throws IOException{
 
+        List<Car> cars = MockData.getCars();
+        List<String> blueCars = cars.stream()
+                .filter(car -> car.getColor().equals("Blue"))
+                .map(car->car.getModel())
+                .collect(Collectors.toList());
+
+        blueCars.forEach(car -> {
+            System.out.println(car);
+        });
+
+        MatcherAssert.assertThat(blueCars, hasItem("MDX"));
     }
 
     @Test
